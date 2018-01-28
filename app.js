119475fa5404420748ab7a6b6139e7b4d681eb81
Jason Mattis <rocknrollx18@aim.com>
2018-01-27 22:42:38 -0800

    Cleaned up code. Moved part of the roll button functionality into a separate function as well as the code that checks whether the player has won the game into a separate function

--- app.js
+++ app.js
@@ -36,17 +36,21 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
-    //3. Update the round score IF the rolled number was not 1
-    if (dice !== 1) {
-      //Add score
-      roundScore += dice;
-      document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-      //Next player
-      nextPlayer();
-    }
+    updateRoundScore(dice);
   }
 });
+
+function updateRoundScore(dice) {
+  //3. Update the round score IF the rolled number was not 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    //Next player
+    nextPlayer();
+  }
+}
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
   if(gamePlaying){
@@ -57,18 +61,22 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     // Check if player won the game
-    if(scores[activePlayer] >= 20){
-      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-      document.querySelector('.dice').style.display = 'none';
-      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-      gamePlaying = false;
-    } else {
-      // Next player
-      nextPlayer();
-    }
+    endOfGame();
   }
 });
+
+function endOfGame() {
+  if(scores[activePlayer] >= 20){
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    gamePlaying = false;
+  } else {
+    // Next player
+    nextPlayer();
+  }
+}
 
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -105,6 +113,3 @@ function init() {
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
 }
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
